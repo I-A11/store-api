61fd91a41b9819637f80e5f2b5cb92cb419afaac
Islam Aboamh <75704011+I-A11@users.noreply.github.com>
2023-05-17 13:49:40 +1000

    Create sort function to sort products.

--- controllers/products.js
+++ controllers/products.js
@@ -1,14 +1,11 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-  const search = "ab";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+  const products = await Product.find({}).sort("-name price");
   res.status(200).json({ products, nbHits: products.length });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -21,9 +18,17 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
-  console.log(queryObject);
+  // console.log(queryObject);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    const sortList = sort.split(",").join(" ");
+    result = result.sort(sortList);
+  } else {
+    result = result.sort("createAt");
+  }
+  const products = await result;
   res.status(200).json({ products, nbHits: products.length });
 };
 
